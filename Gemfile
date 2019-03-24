fbf2084ff806ee1a82aafafafb11290475e58b4a
Seong Ryong Koh <johann@taeufer.kjhome>
2019-03-24 22:47:42 +0100

    InitVersion

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source "https://rubygems.org"
 
-gem "github-pages", group: :jekyll_plugins
+#gem "github-pages", group: :jekyll_plugins
+#source "https://rubygems.org"
+
+gem "jekyll", "~> 3.5"
+gem "minimal-mistakes-jekyll"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "피정 자료",
+        "excerpt":"2019년 4월 12일-13일 피정자료  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Jubilaeum-Retreat/",
+        "teaser":null}]
