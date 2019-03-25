1230a36746117cfa83d4997fd60f68e775acf98f
Seong Ryong Koh <johann@taeufer.kjhome>
2019-03-25 10:12:51 +0100

    modified:   Gemfile.lock 	modified:   _config.yml 	modified:   _data/navigation.yml 	deleted:    _posts/2019-03-20-Jubilaeum-Retreat.md 	deleted:    _site/Jubilaeum-Retreat/index.html 	modified:   _site/assets/css/main.css 	deleted:    _site/assets/js/_main.js 	modified:   _site/assets/js/lunr/lunr-store.js 	modified:   _site/assets/js/main.min.js 	deleted:    _site/assets/js/plugins/jquery.ba-throttle-debounce.js 	deleted:    _site/assets/js/plugins/jquery.fitvids.js 	deleted:    _site/assets/js/plugins/jquery.greedy-navigation.js 	deleted:    _site/assets/js/plugins/jquery.magnific-popup.js 	deleted:    _site/assets/js/plugins/jquery.smooth-scroll.min.js 	deleted:    _site/assets/js/vendor/jquery/jquery-3.3.1.min.js 	modified:   _site/feed.xml 	modified:   _site/index.html 	modified:   _site/portfolio/index.html 	modified:   _site/sitemap.xml 	.sass-cache/ 	_includes/ 	_layouts/ 	_pages/404.md 	_pages/archive-layout-with-content.md 	_pages/category-archive.md 	_pages/collection-archive.html 	_pages/edge-case.md 	_pages/lorem-ipsum.md 	_pages/markup.md 	_pages/page-a.md 	_pages/page-archive.html 	_pages/page-b.md 	_pages/post-archive-feature-rows.html 	_pages/recipes-archive.md 	_pages/sample-page.md 	_pages/search.md 	_pages/sitemap.md 	_pages/splash-page.md 	_pages/tag-archive.md 	_pages/terms.md 	_pages/year-archive.md 	_posts/ 	_sass/ 	_site/404.html 	_site/archive-layout-with-content/ 	_site/assets/images/ 	_site/categories-archive/ 	_site/categories/ 	_site/collection-archive/ 	_site/jubilaeum-retreat/ 	_site/lorem-ipsum/ 	_site/page-a/ 	_site/page-archive/ 	_site/page-b/ 	_site/post-archive-feature-rows/ 	_site/recipes-archive/ 	_site/sample-page/ 	_site/search/ 	_site/sitemap/ 	_site/splash-page/ 	_site/tags-archive/ 	_site/tags/ 	_site/terms/ 	_site/year-archive/ 	assets/

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "피정 자료",
-        "excerpt":"2019년 4월 12일-13일 피정자료  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/Jubilaeum-Retreat/",
+        "title": "50주년 준비위원회 피정 자료",
+        "excerpt":"준비위원회 첫번째 피정      일시: 2019년 4월 12일 - 13일   장소: Haus Venusberg   쾰른 한인 천주교회 설립 50주년을 맞이하는 2020년을 기념하기 위하여 지난 2018년 준비위원회를 조직하였습니다. 그동안 진행해왔던 프로그램들과 함께 강연과 미사, 그리고 일반 기념 사업에 대한 구체적인 의논이 진행될 첫 모임입니다.   2019년 4월 12일-13일 피정자료  ","categories": [],
+        "tags": ["kakoka","jubilaeum","slides"],
+        "url": "http://localhost:4000/jubilaeum-retreat/",
         "teaser":null}]
